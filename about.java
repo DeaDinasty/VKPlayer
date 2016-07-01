f74f97d93c57a6d679385de62da103dccc8dcc37
DeaDinasty <andrey.swift@gmail.com>
2016-07-01 14:57:37 +0000

    Zalupa

--- about.java
+++ about.java
@@ -22,18 +22,25 @@ class About{
 					Pattern p = Pattern.compile("^(.)*TPE1\\W");
 					Matcher m = p.matcher(s);
 					if (m.find()) {
-						s = m.group().substring(0, m.group().length() - 5);
+						s = m.group().substring(0, m.group().length() - 8);
 						file_names[i] = s;
 						System.out.println("Name: " + s);
 					}
 				}
-				System.out.println();
 			} catch(IOException e) {
 				System.out.println("File not found!");
 			}
-		}
-		for (int i = 0; i < files.length; i++) {
-			
+		} //Можно объединить циклы
+		for (int i = 0; i < file_names.length; i++) {
+			System.out.println(file_names[i]);
+			String name = file_names[i] + ".mp3";
+			System.out.println(PATH + name);
+			File newFile = new File(PATH + name);
+			try {
+				newFile.createNewFile();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 } 
